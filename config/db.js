73e1b9f332ce24d76000e04c7beef0de7a4ad823
Aayush <aayushc320sm@gmail.com>
2023-07-23 14:06:14 +0530

    Project Added

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import colors from 'colors';
+const connectDB= async()=>{
+    try{
+        const conn= await mongoose.connect(process.env.MONGO_URL);
+        console.log(`Connected to Mongo-DB database ${conn.connection.host}`);
+    } catch(error){
+        console.log(`Error in MongoDb ${error}`.bgRed.white);
+    }
+}
+export default connectDB;
